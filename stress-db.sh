76684b53aa72bf31cfa5ba329292b3364fa86459
korti_mortada@yahoo.com <korti_mortada@yahoo.com>
2025-04-05 19:08:16 +0200

    Added executable script to stress redis database

---
+++ stress-db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "📊 Simulation de charge sur Redis..."
+
+# Set une clé dans le master
+echo "📥 Insertion de la clé dans redis-master..."
+kubectl exec deploy/redis-master -- redis-cli set testkey "KORTI-BENKACI"
+
+echo "🛑 Appuyez sur Ctrl+C pour arrêter la simulation."
+
+# Boucle infinie de lecture
+kubectl exec -it deploy/redis-replica -- sh -c 'while true; do redis-cli get testkey; done'
+
+echo "✅ Simulation terminée."
